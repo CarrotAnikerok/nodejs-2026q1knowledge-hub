d633f8179d3874190a9b8a269b700c055807c0ca
carrot <viktoriya.korekina@simpleone.ru>
2026-04-10 12:32:10 +0300

    feat: add seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient, Role, Status } from '../src/generated/prisma/client';
-import { create } from 'domain';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
@@ -102,14 +101,111 @@ async function main() {
     update: {},
     create: {
       id: '3117c6ed-b065-4d4f-91ca-3217bc008e76',
-      title: 'First asrticle',
-      content: 'harry potter',
+      title: 'First article',
+      content: 'first',
       status: Status.ARCHIVED,
       authorId: admin.id,
       categoryId: books.id,
       tags: {
         connect: [{ name: 'MLP' }],
       },
+    },
+  });
+
+  await prisma.article.upsert({
+    where: { id: 'e92ae348-ecb0-4713-8550-4c2ab0bfcc08' },
+    update: {},
+    create: {
+      id: 'e92ae348-ecb0-4713-8550-4c2ab0bfcc08',
+      title: 'Second article',
+      content: 'second',
+      status: Status.DRAFT,
+      authorId: admin.id,
+      categoryId: magic.id,
+      tags: {
+        connect: [{ name: 'Love' }, { name: 'Gravity Falls' }],
+      },
+    },
+  });
+
+  await prisma.article.upsert({
+    where: { id: '2db5ae9f-eada-4fef-8c95-9dbb33e7ada5' },
+    update: {},
+    create: {
+      id: '2db5ae9f-eada-4fef-8c95-9dbb33e7ada5',
+      title: 'Third article',
+      content: 'third',
+      status: Status.DRAFT,
+      authorId: editor.id,
+      categoryId: sports.id,
+      tags: {
+        connect: [{ name: 'Plants' }],
+      },
+    },
+  });
+
+  const fourthArticle = await prisma.article.upsert({
+    where: { id: '2db5ae9f-eada-4fef-8c95-9dbb33e7ada5' },
+    update: {},
+    create: {
+      id: '2db5ae9f-eada-4fef-8c95-9dbb33e7ada5',
+      title: 'Fourth article',
+      content: 'fourth',
+      status: Status.PUBLISHED,
+      authorId: editor.id,
+      categoryId: books.id,
+      tags: {
+        connect: [{ name: 'Harry Potter' }],
+      },
+    },
+  });
+
+  const fifthArticle = await prisma.article.upsert({
+    where: { id: '02975bd7-406c-4d84-a908-799c6161fc1b' },
+    update: {},
+    create: {
+      id: '02975bd7-406c-4d84-a908-799c6161fc1b',
+      title: 'Fifth article',
+      content: 'fifth',
+      status: Status.PUBLISHED,
+      authorId: editor.id,
+      categoryId: books.id,
+      tags: {
+        connect: [{ name: 'MLP' }],
+      },
+    },
+  });
+
+  await prisma.comment.upsert({
+    where: { id: 'bf9c8eff-d7c6-44c4-9733-3180bbf44ecd' },
+    update: {},
+    create: {
+      id: 'bf9c8eff-d7c6-44c4-9733-3180bbf44ecd',
+      content: 'first comment',
+      authorId: editor.id,
+      articleId: firstArticle.id,
+    },
+  });
+
+  await prisma.comment.upsert({
+    where: { id: '63420af1-aa4e-4205-855e-60cf26076251' },
+    update: {},
+    create: {
+      id: '63420af1-aa4e-4205-855e-60cf26076251',
+      content: 'second comment',
+      authorId: admin.id,
+      articleId: fifthArticle.id,
+    },
+  });
+
+  await prisma.comment.upsert({
+    where: { id: 'add9e57d-ca0c-4109-ad35-50ab394bb6fe' },
+    update: {},
+    create: {
+      id: 'add9e57d-ca0c-4109-ad35-50ab394bb6fe',
+      content: 'third comment',
+      authorId: admin.id,
+      articleId: fourthArticle.id,
     },
   });
 
